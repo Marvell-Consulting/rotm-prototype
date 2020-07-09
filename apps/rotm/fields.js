52c12e2018cc4047938b5bb5583286d8a6bc65fc
Mike Thomas <mikieet6@gmail.com>
2020-07-09 17:26:32 +0100

    Removing unused code

--- apps/rotm/fields.js
+++ apps/rotm/fields.js
@@ -8,32 +8,6 @@ module.exports = {
   source: {
     validate: 'required'
   },
-  'more-info': {
-    mixin: 'textarea'
-  },
-  'report-reason': {
-    mixin: 'textarea',
-    legend: {
-      className: 'visuallyhidden'
-    }
-  },
-  'add-image': {
-    mixin: 'radio-group',
-    validate: 'required',
-    legend: {
-      className: 'visuallyhidden'
-    },
-    options: [
-      'yes',
-      'no'
-    ]
-  },
-  'who-shared': {
-    mixin: 'input-text',
-    legend: {
-      className: 'visuallyhidden'
-    }
-  },
   'can-we-contact': {
     mixin: 'radio-group',
     validate: 'required',
@@ -79,15 +53,6 @@ module.exports = {
       return extensions.includes(path.extname(value));
     }]
   },
-  'image-paste': {
-    mixin: 'input-text',
-    legend: {
-      className: 'visuallyhidden'
-    }
-  },
-  'evidence-general': {
-    mixin: 'textarea'
-  },
   'evidence-written': {
     mixin: 'textarea',
     attributes: [{
@@ -114,41 +79,6 @@ module.exports = {
     attributes: [{
       attribute: 'rows',
       value: 1
-    }]
-  },
-  'evidence-url-auto': {
-    mixin: 'radio-group',
-    validate: 'required',
-    legend: {
-      className: 'visuallyhidden'
-    },
-     options: [{
-      value: 'yes'
-    }, {
-      value: 'no'
-    }]
-  },
-  'send-copy': {
-    mixin: 'checkbox-group',
-    options: [{
-      value: 'yes',
-      toggle: 'send-copy-email',
-      child: 'input-text'
-    }]
-  },
-
-  'video': {
-    mixin: 'radio-group',
-    validate: 'required',
-    legend: {
-      className: 'visuallyhidden'
-    },
-    options: [{
-      value: 'yes',
-      toggle: 'video-upload',
-      child: 'input-file'
-    }, {
-      value: 'no'
     }]
   },
   'contact-details-name': {
